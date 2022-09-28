54bd6f3361cb3209dd0d4d492af89a5e488c69e7
TyphoonEvan <evanroscorla@gmail.com>
2022-09-28 18:36:03 +0100

    started pandas testing

--- other/pandastest.py
+++ other/pandastest.py
@@ -8,9 +8,9 @@ data = json.loads(r.content)
 dataframe = pd.json_normalize(data["history"])
 refinedframe = dataframe[["element", "total_points", "goals_scored", "assists", "minutes", "round"]]
 
-def getPoints(dataframe):
+def points(dataframe):
     meanpoints = dataframe["total_points"].mean()
-    currentpoints = dataframe.iloc[dataframe.shape[0]-1, 1]
+    currentpoints = dataframe.iloc[dataframe.shape[0], 1]
     if currentpoints > meanpoints:
         return "+"
     elif currentpoints == meanpoints:
@@ -18,11 +18,9 @@ def getPoints(dataframe):
     else:
         return "-"
 
-print(getPoints(refinedframe))
-
-def getTrend(dataframe):
+def trend(dataframe):
     trend = []
-    for i in range(dataframe.shape[0]-1):
+    for i in range(dataframe.shape[0]):
         currentval = dataframe.iloc[i, 1]
         try:
             prevval = dataframe.iloc[i-1, 1]
@@ -32,4 +30,13 @@ def getTrend(dataframe):
         trend.append(difference)
     return trend
 
-print(getTrend(refinedframe))
+def playTime(dataframe):
+    averagetime = dataframe["minutes"].mean()
+    played = 0
+    notplayed = 0
+    for i in range(dataframe.shape[0]):
+        if dataframe.iloc[i, 4] == 0:
+            notplayed+=1
+        else:
+            played+=1
+    return averagetime, played, notplayed
